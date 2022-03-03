e7ec872be8d8c1a8557b2fcebce409ffb395dbc2
Qinyuan Ye <qinyuany@usc.edu>
2022-03-03 12:01:44 -0800

    keeping code for transformers 4.5.0

--- bart.py
+++ bart.py
@@ -3,32 +3,70 @@ import torch.nn.functional as F
 from torch import Tensor, nn
 from transformers import T5ForConditionalGeneration, BartForConditionalGeneration
 from transformers.models.bart.modeling_bart import shift_tokens_right
+from transformers.modeling_outputs import Seq2SeqLMOutput
 
 from utils import label_smoothed_nll_loss
 
 class MyBart(BartForConditionalGeneration):
-    def forward(self, input_ids, attention_mask=None, encoder_outputs=None,
-            decoder_input_ids=None, decoder_attention_mask=None, decoder_cached_states=None,
-            use_cache=False, is_training=False):
+    def forward(
+        self,
+        input_ids=None,
+        attention_mask=None,
+        decoder_input_ids=None,
+        decoder_attention_mask=None,
+        head_mask=None,
+        decoder_head_mask=None,
+        encoder_outputs=None,
+        past_key_values=None,
+        inputs_embeds=None,
+        decoder_inputs_embeds=None,
+        use_cache=None,
+        output_attentions=None,
+        output_hidden_states=None,
+        return_dict=None,
+        is_training=False,
+        ):
 
         if is_training:
-            _decoder_input_ids = shift_tokens_right(decoder_input_ids, self.config.pad_token_id)
+            _decoder_input_ids = shift_tokens_right(decoder_input_ids, self.config.pad_token_id, self.config.decoder_start_token_id)
         else:
             _decoder_input_ids = decoder_input_ids
 
         outputs = self.model(
-            input_ids,
+            input_ids=input_ids,
             attention_mask=attention_mask,
-            encoder_outputs=encoder_outputs,
             decoder_input_ids=_decoder_input_ids,
             decoder_attention_mask=decoder_attention_mask,
-            decoder_cached_states=decoder_cached_states,
+            head_mask=head_mask,
+            decoder_head_mask=decoder_head_mask,
+            encoder_outputs=encoder_outputs,
+            past_key_values=past_key_values,
+            inputs_embeds=inputs_embeds,
+            decoder_inputs_embeds=decoder_inputs_embeds,
             use_cache=use_cache,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
         )
+
+        # lm_logits = F.linear(outputs[0], self.model.shared.weight, bias=self.final_logits_bias)
         lm_logits = F.linear(outputs[0], self.model.shared.weight, bias=self.final_logits_bias)
+        lprobs = F.log_softmax(lm_logits, dim=-1)
+        masked_lm_loss , _ = label_smoothed_nll_loss(lprobs, decoder_input_ids, epsilon=0.1, ignore_index=self.config.pad_token_id)
+        
         if is_training:
-            lprobs = F.log_softmax(lm_logits, dim=-1)
-            loss, _ = label_smoothed_nll_loss(lprobs, decoder_input_ids, epsilon=0.1, ignore_index=self.config.pad_token_id)
-            return loss
-        return (lm_logits, ) + outputs[1:]
+            return masked_lm_loss 
+
+        return Seq2SeqLMOutput(
+            loss=masked_lm_loss,
+            logits=lm_logits,
+            past_key_values=outputs.past_key_values,
+            decoder_hidden_states=outputs.decoder_hidden_states,
+            decoder_attentions=outputs.decoder_attentions,
+            cross_attentions=outputs.cross_attentions,
+            encoder_last_hidden_state=outputs.encoder_last_hidden_state,
+            encoder_hidden_states=outputs.encoder_hidden_states,
+            encoder_attentions=outputs.encoder_attentions,
+        )
+        # return (lm_logits, ) + outputs[1:]
 
